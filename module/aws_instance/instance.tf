479e17e6e0f74fef11183ecde89db8007887ac88
Jignesh <jignesh.9202@gmail.com>
2022-08-11 14:48:59 +0530

    updated instance file

--- module/aws_instance/instance.tf
+++ module/aws_instance/instance.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "demo_instance" {
-  ami = "${lookup(var.ami_id, var.region, "us-east-1")}"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "demo_instance"
-  }
-}
+# resource "aws_instance" "demo_instance" {
+#  ami = "${lookup(var.ami_id, var.region, "us-east-1")}"
+#  instance_type = "t2.micro"
+#
+#  tags = {
+#    Name = "demo_instance"
+#  }
+#}
